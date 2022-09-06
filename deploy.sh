45229ca7a4296e1191d42830f84d8128a360931e
vicmms <victormmsauz@gmail.com>
2022-09-06 10:43:05 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ cd dist
 git init
 git add -A 
 git commit -m 'New deployment'
-git push -f git@github.com:vicmms/webapp--easygoband.git main:gh-pages
+git push -f git@github.com:vicmms/webapp--easygoband.git master:gh-pages
 
 cd -
 
