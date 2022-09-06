7e7cbd8e4a039ec71738e453d5598135da1e62a7
vicmms <victormmsauz@gmail.com>
2022-09-06 02:38:38 -0500

    add config page

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
 
-set -env
+set -e
 
 npm run build
 
 cd dist
 
 git init
-git add -addgit commit -m 'New deployment'
+git add -A 
+git commit -m 'New deployment'
 git push -f git@github.com:vicmms/webapp--easygoband.git main:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/webapp--easygoband/' : '/',
+  publicPath: '/webapp--easygoband/',
   transpileDependencies: [
     'vuetify'
   ]
